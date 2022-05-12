74125b349b9da32419fc3167af863c7e4775ab95
Joao Paulo Dias <jpdias1997@hotmail.com>
2022-05-11 21:24:21 -0300

    test: creation of validation 5

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -4,13 +4,15 @@ import { PostUserRequestBuilder } from "../builders/post-user-request.builder";
 import { PutUserRequestBuilder } from "../builders/put-user-request.builder";
 import { GetUserResponse } from "../model/get-user-response.model";
 import { PostUserRequest } from "../model/post-user-request.model";
-import { PutUserRequest } from "../model/put-user-request.model";
 import { PutUserResponse } from "../model/put-user-response.model";
+import times from "async/times";
+import { performance } from "perf_hooks";
+
 const request = client("https://reqres.in/api");
 const getUserSerializer = new TypedJSON(GetUserResponse);
 const postUserSerializer = new TypedJSON(PostUserRequest);
 const putUserSerializer = new TypedJSON(PutUserResponse);
-import { performance } from 'perf_hooks';
+
 describe("API Validations", () => {
   describe("/users", () => {
     test("should get list of users", async () => {
@@ -21,28 +23,41 @@ describe("API Validations", () => {
     });
 
     test("should create an user", async () => {
-        const today = new Date().getDate();
-        const response = await request.post("/users").send(PostUserRequestBuilder.new().build());
-        expect(response.status).toBe(201);
-        const user = postUserSerializer.parse(response.body);
-        expect(user).toBeTruthy();
-        expect(user.createdAt.getDate()).toBe(today)
-      });
+      const today = new Date().getDate();
+      const response = await request
+        .post("/users")
+        .send(PostUserRequestBuilder.new().build());
+      expect(response.status).toBe(201);
+      const user = postUserSerializer.parse(response.body);
+      expect(user).toBeTruthy();
+      expect(user.createdAt.getDate()).toBe(today);
+    });
 
-      test("should update an user", async () => {
-        const userToUpdate = PutUserRequestBuilder.new().build();
-        const {id, ...userExpected} = userToUpdate;
-        const response = await request.put(`/users/${id}`).send(userExpected);
-        expect(response.status).toBe(200);
-        const userUpdated = putUserSerializer.parse(response.body);
-        expect(userExpected).toEqual(userUpdated);
-      });
+    test("should update an user", async () => {
+      const userToUpdate = PutUserRequestBuilder.new().build();
+      const { id, ...userExpected } = userToUpdate;
+      const response = await request.put(`/users/${id}`).send(userExpected);
+      expect(response.status).toBe(200);
+      const userUpdated = putUserSerializer.parse(response.body);
+      expect(userExpected).toEqual(userUpdated);
+    });
 
-      test.each<Number>([0,3])("should get list of users of the page %i with response time less than 1 second", async (page:number) => {
+    test.each<Number>([0, 3])(
+      "should get list of users of the page %i with response time less than 1 second",
+      async (page: number) => {
         const startTime = performance.now();
         await request.get(`/users?page=${page}`);
-        const executionTimeInMiliseconds = performance.now() - startTime
+        const executionTimeInMiliseconds = performance.now() - startTime;
         expect(executionTimeInMiliseconds).toBeLessThan(1000);
-      });
+      }
+    );
+
+    test("should get list of users 10 times parallelly and validate status 200 in each of them", async () => {
+
+      let asyncTasks = async () => {
+        await request.get("/users").expect(200);
+      };
+      await times(10, asyncTasks);
+    });
   });
 });
